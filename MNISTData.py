12e800e3ab1677645b7c3e1700f029314d3e1b18
Andre Mendes <andre.f.mendes24@gmail.com>
2022-12-23 19:08:20 +0000

    Processing and displaying MNIST dataset

---
+++ MNISTData.py
@@ -0,0 +1,29 @@
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def loadMNIST(images_path, labels_path):
+    with open(labels_path, "rb") as lbpath:
+        magic, n = struct.unpack(">II", lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
+    with open(images_path, "rb") as imgpath:
+        magic, num, rows, cols = struct.unpack(">IIII", imgpath.read(16))
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 28, 28)
+
+    return images, labels
+
+
+def showMNIST(X, y, n_samples):
+    images = X[:n_samples]
+    labels = y[:n_samples]
+
+    num_row = 2
+    num_col = n_samples//2
+    fig, axes = plt.subplots(num_row, num_col, figsize=(1.5*num_col, 2*num_row))
+    for i in range(n_samples):
+        ax = axes[i//num_col, i % num_col]
+        ax.imshow(images[i], cmap='gray_r')
+        ax.set_title('Label: {}'.format(labels[i]))
+    plt.tight_layout()
+    plt.show()
