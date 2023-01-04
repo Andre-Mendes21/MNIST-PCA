0e25a398930610cd137334d26dfeacb67d39c22e
Andre Mendes <andre.f.mendes24@gmail.com>
2023-01-04 14:02:13 +0000

    Added way to select the eigen values in PCA func

--- main.py
+++ main.py
@@ -25,22 +25,33 @@ def find_elbow(eigen_vals, trace, goal_confidence):
     return k, confidence
 
 
+def calc_confidence(eigen_vals, trace, k):
+    confidence = 0
+    for i in range(k):
+        confidence += eigen_vals[k] / trace
+    return confidence
+
+
 def calc_coeficiente_proj(e_digits, centred_X, X_len):
     coef_proj = [np.dot(centred_X[i], e_digits) for i in range(X_len)]
     return coef_proj
 
 
-def PCA(X):
+def PCA(X, goal_confidence, k=None):
     mean_X = np.mean(X, 0)
     centred_X = X - mean_X
     e_digits, singular_values, V = linalg.svd(centred_X.transpose(), full_matrices=False)
     eigen_vals = singular_values * singular_values
     trace = sum(eigen_vals)
-    k, confidence = find_elbow(eigen_vals, trace, GOAL_CONFIDENCE)
-    print(f'k: {k}\nconfidence: {confidence}')
-    # show_eigen_vals(len(X), eigen_vals, k)
+
+    if k == None:
+        k, confidence = find_elbow(eigen_vals, trace, goal_confidence)
+    else:
+        confidence = calc_confidence(eigen_vals, trace, k)
+    print(f'k: {k}\nconfidence: {confidence}\neigen_vals len: {len(eigen_vals)}')
+
     coef_proj = calc_coeficiente_proj(e_digits, centred_X, len(X))
-    return mean_X, e_digits, eigen_vals, coef_proj
+    return mean_X, e_digits, eigen_vals[:k], coef_proj
 
 
 def euclidean_dist(coef_proj, test_coef_proj, X_len, eigen_vals=None):
@@ -71,15 +82,16 @@ if __name__ == '__main__':
                             'data/training/train-labels.idx1-ubyte')
     test_X, test_y = MNISTData.loadMNIST('data/test/t10k-images.idx3-ubyte', 
                                         'data/test/t10k-labels.idx1-ubyte')
-    # MNISTData.showMNIST(X, y, 14)
-    mean_X, e_digits, eigen_vals, coef_proj = PCA(X)
+    mean_X, e_digits, eigen_vals, coef_proj = PCA(X, GOAL_CONFIDENCE, 3)
+    print(f'eigen_vals len: {len(eigen_vals)}')
     tests_passed = 0
     for i in range(len(test_X)):
         d_min, d_arg_min = identify(test_X[i], mean_X, e_digits, eigen_vals, coef_proj, len(X), euclidean_dist)
         if test_y[i] != y[d_arg_min]:
-            print(f'i: {i} expected: {test_y[i]}\ngot: {y[d_arg_min]}')
+            print(f'i: {i} expected: {test_y[i]} got: {y[d_arg_min]}')
         else:
             tests_passed += 1
-            current_percent = tests_passed / len(test_X)
+            current_percent = tests_passed / (len(test_X))
+            print(f'Current Percent: {current_percent * 100}')
 
-    print(f'Passed Tests %: {current_percent}')
+    print(f'Passed Tests: {current_percent}')
