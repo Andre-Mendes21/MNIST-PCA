b5f60892f208d8beaca28eb4f9ac7a49e4e04c1c
Andre Mendes <andre.f.mendes24@gmail.com>
2023-01-05 12:00:12 +0000

    Fixed mahalnobis distance

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
-GOAL_CONFIDENCE = 0.95
+GOAL_CONFIDENCE = 0.75
 
 
 class PCA:
@@ -34,7 +34,7 @@ class PCA:
     def pca(self):
         self.mean_X = np.mean(self.X, 0)
         centred_X = self.X - self.mean_X
-        self.e_digits, singular_values, V = linalg.svd(centred_X.transpose(), full_matrices=False)
+        self.e_digits, singular_values, _ = linalg.svd(centred_X.transpose(), full_matrices=False)
         self.all_eigen_vals = singular_values * singular_values
         trace = sum(self.all_eigen_vals)
 
@@ -50,8 +50,8 @@ class PCA:
 
 def show_eigen_vals(pca: PCA):
     plt.figure(figsize=(10, 10))
-    t = np.arange(0, pca.X_len, 1)
-    plt.plot(t, pca._eigen_vals, 'x')
+    t = np.arange(0, len(pca.all_eigen_vals), 1)
+    plt.plot(t, pca.all_eigen_vals, 'x')
     plt.plot(pca.k, pca.all_eigen_vals[pca.k], 'o')
     plt.xlabel('K')
     plt.ylabel('Eigen values')
@@ -62,14 +62,16 @@ def euclidean_dist(pca: PCA, test_coef_proj):
     return [linalg.norm(pca.coef_proj[i] - test_coef_proj) for i in range(pca.X_len)]
 
 
-# Does not work as intended
 def mahalanobis_dist(pca: PCA, test_coef_proj):
-    dist = []
-    for i in range(pca.k):
-        inv_eigen_val = 1/pca.eigen_vals[i]
-        diff_sqr = np.square(pca.coef_proj[i] - test_coef_proj[i])
-        dist = np.append(dist, inv_eigen_val * diff_sqr)
-    return dist
+    dists = []
+    dist = 0
+    for i in range(pca.X_len):
+        for j in range(pca.k):
+            inv_eigen_val = 1/pca.eigen_vals[j]
+            diff_sqr = np.square(pca.coef_proj[i][j] - test_coef_proj[j])
+            dist += inv_eigen_val * diff_sqr
+        dists = np.append(dists, dist)
+    return dists
 
 
 def identify(pca: PCA, test_X, dist_func):
@@ -88,12 +90,12 @@ if __name__ == '__main__':
                                         'data/test/t10k-labels.idx1-ubyte')
     test_len = len(test_X)
 
-    pca = PCA(train_X, GOAL_CONFIDENCE, 3)
+    pca = PCA(train_X, GOAL_CONFIDENCE, 28)
     pca.pca()
     dists = []
     tests_passed = 0
     for i in range(test_len):
-        d_min, d_arg_min = identify(pca, test_X[i], euclidean_dist)
+        d_min, d_arg_min = identify(pca, test_X[i], mahalanobis_dist)
         if test_y[i] != train_y[d_arg_min]:
             dists = np.append(dists, d_min)
             avg_dist = np.mean(dists)
