65d5e4a80027fff4a700c2e4b29065dac8300352
Andre Mendes <andre.f.mendes24@gmail.com>
2023-01-08 23:03:31 +0000

    Add noise function for images

---
+++ noise.py
@@ -0,0 +1,20 @@
+import MNISTData
+import numpy as np
+
+
+def add_noise(X, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    mu, sigma = 0, 1
+    noisey_images = []
+    noise = np.random.normal(mu, sigma, size=X[0].shape)
+    for i in range(len(X)):
+        noisy = np.clip((X[i] + noise * 0.2), 0, 1)
+        noisey_images.append(noisy)
+    return noisey_images
+
+
+def noisey_MNIST(images_path, labels_path):
+    X, y = MNISTData.loadMNIST(images_path, labels_path)
+    noise_X = add_noise(X)
+    return noise_X, y
