ce0ddf8a6d941ac83779e965999efce6aac4e5de
Andre Mendes <andre.f.mendes24@gmail.com>
2023-01-13 16:24:16 +0000

    Updated noise.py for better noise function

--- noise.py
+++ noise.py
@@ -1,13 +1,10 @@
-import numpy as np
+from skimage import util
 
 
 def add_noise(X, seed=None):
-    if seed is not None:
-        np.random.seed(seed)
-    mu, sigma = 0, 1
     noisy_images = []
-    noise = np.random.normal(mu, sigma, size=X[0].shape)
     for i in range(len(X)):
-        noisy = np.clip((X[i] + noise * 0.2), 0, 1)
+        noisy = util.random_noise(X[i], mode='gaussian', seed=seed,
+                                  clip=True, var=0.035)
         noisy_images.append(noisy)
     return noisy_images
