46027cb9e0d935cbddd283370f72f56445f6fee3
Andre Mendes <andre.f.mendes24@gmail.com>
2023-01-15 07:17:26 +0000

    Fixed noise function to work with MNIST format

--- noise.py
+++ noise.py
@@ -1,3 +1,4 @@
+import numpy as np
 from skimage import util
 
 
@@ -6,5 +7,6 @@ def add_noise(X, seed=None):
     for i in range(len(X)):
         noisy = util.random_noise(X[i], mode='gaussian', seed=seed,
                                   clip=True, var=0.035)
+        noisy = np.floor(np.clip(noisy * 255, 0, 255))
         noisy_images.append(noisy)
     return noisy_images
